fdc0371c5aa75bea3b78279311d878260368eb86
ilathem <isaiahlathem@gmail.com>
2024-04-24 19:42:07 -0500

    Setup initial api caller

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./index.css";
+
+const img = document.querySelector("img");
+
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=984QnTym9Qxyr6BerKBTItNptXmUaj2i&s=cats",
+  {
+    mode: "cors",
+  },
+)
+  .then((res) => res.json())
+  .then((json) => img.src = json.data.images.original.url);
